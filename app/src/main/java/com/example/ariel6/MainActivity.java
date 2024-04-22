749ef074c9f797ba97b46abde4272a1c99dcb584
JustCoding124 <99827982+JustCoding124@users.noreply.github.com>
2024-04-22 15:26:58 +0300

    2

--- app/src/main/java/com/example/ariel6/MainActivity.java
+++ app/src/main/java/com/example/ariel6/MainActivity.java
@@ -1,6 +1,14 @@
 package com.example.ariel6;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.util.Random;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +17,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText a;
+    private EditText b;
+    private EditText c;
+    private double av;
+    private double bv;
+    private double cv;
+    private Button calc;
+    private Button rand;
+    private TextView result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +36,54 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        a= (EditText) findViewById(R.id.A);
+        b= (EditText) findViewById(R.id.B);
+        c= (EditText) findViewById(R.id.C);
+        calc= (Button) findViewById(R.id.calc);
+        rand= (Button) findViewById(R.id.rand);
+        result= (TextView) findViewById(R.id.result);
+
+
+    }
+    public void randomize(View view){
+        Random rand = new Random();
+        av = rand.nextInt(1000);
+        bv = rand.nextInt(1000);
+        cv = rand.nextInt(1000);
+        a.setText(av +"");
+        b.setText(bv +"");
+        c.setText(cv +"");
+    }
+    public void calculate(View view){
+        try {
+            av = Double.parseDouble(a.getText().toString());
+            bv = Double.parseDouble(b.getText().toString());
+            cv = Double.parseDouble(c.getText().toString());
+        }   catch (Exception e){
+            a.setText("Something Went Wrong");
+            b.setText("Something Went Wrong");
+            c.setText("Something Went Wrong");
+            return;
+        }
+        Intent si = new Intent(this, MainActivity2.class);
+        si.putExtra("n",av);
+        si.putExtra("nn",bv);
+        si.putExtra("nnn",cv);
+        startActivityForResult(si,1);
+    }
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if (requestCode==1){
+            if (resultCode== Activity.RESULT_OK)
+            {
+                String result1 = data.getStringExtra("result1");
+                String result2 = data.getStringExtra("result2");
+                if (result1.equals("Complex Root")||result2.equals("Complex Root"))
+                    result.setText("Complex Root");
+                else
+                    result.setText(result1+"\n"+result2);
+
+            }
+        }
     }
 }
--- app/src/main/java/com/example/ariel6/MainActivity2.java
+++ app/src/main/java/com/example/ariel6/MainActivity2.java
@@ -1,6 +1,11 @@
 package com.example.ariel6;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +14,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    private Button back;
+    private ImageView smiling;
+    private ImageView crying;
+    private Intent gi;
+    private TextView result1;
+    private TextView result2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +30,45 @@ public class MainActivity2 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        result1 = (TextView) findViewById(R.id.result1);
+        result2 = (TextView) findViewById(R.id.result2);
+        crying = (ImageView) findViewById(R.id.crying2);
+        smiling = (ImageView) findViewById(R.id.smiling);
+        back = (Button)findViewById(R.id.back);
+        crying.setVisibility(View.INVISIBLE);
+        smiling.setVisibility(View.INVISIBLE);
+    }
+
+    public void calculate() {
+        gi = getIntent();
+        double a = gi.getDoubleExtra("n", 1);
+        double b = gi.getIntExtra("nn", 1);
+        double c = gi.getIntExtra("nnn", 1);
+        if (a > 0)
+            smiling.setVisibility(View.VISIBLE);
+        else
+            crying.setVisibility(View.VISIBLE);
+        double discriminant = b * b - 4 * a * c;
+        if (discriminant > 0) {
+            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            result1.setText(root1 + "");
+            result2.setText(root2 + "");
+        } else if (discriminant == 0) {
+            // One real root (repeated)
+            double root = -b / (2 * a);
+            result2.setText(root + "");
+            result1.setText("Only 1 solution");
+        } else {
+            result1.setText("Complex Root");
+            result2.setText("Complex Root");
+        }
+    }
+    public void back(){
+        gi.putExtra("result1", result1.getText().toString());
+        gi.putExtra("result2", result2.getText().toString());
+        setResult(RESULT_OK,gi);
+        finish();
+
     }
 }
